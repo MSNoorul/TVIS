f2551f4540a068dfa523090e93fe68233ed43616
Noorul ameen <noorulameen544@gmail.com>
2026-08-08 10:34:19 +0530

    fix : routing issues

--- src/App.tsx
+++ src/App.tsx
@@ -36,8 +36,9 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
+             <ChatWidget />
         </BrowserRouter>
-        <ChatWidget />
+     
       </TooltipProvider>
     </LanguageProvider>
   </QueryClientProvider>
